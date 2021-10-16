1d85f9c72bb7aa0c5fdccf71098f4efbf9d99bb3
khaitq <khaitq@cystack.net>
2021-10-16 11:22:23 +0700

    Add cron

---
+++ src/cron/controllers/utils/django_config.py
@@ -0,0 +1,20 @@
+import os
+import django
+import traceback
+
+from cron.controllers.utils.logger import logger
+
+
+def django_config():
+    try:
+        valid_env = ['prod', 'env', 'staging']
+        env = os.getenv("ENVIRONMENT")
+        if env not in valid_env:
+            env = 'prod'
+        setting = "server_config.settings.%s" % env
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", setting)
+
+        django.setup()
+    except:
+        tb = traceback.format_exc()
+        logger.error(tb)
