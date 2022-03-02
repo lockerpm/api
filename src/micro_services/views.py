acdd5e54c133efba966a942398b04182f205bc2e
Tran Quang Khai <khai.tranquang.8797@gmail.com>
2022-03-02 15:46:48 +0700

    Update mobile payment webhook

--- src/micro_services/views.py
+++ src/micro_services/views.py
@@ -1,3 +1,4 @@
 from micro_services.sync.teams.views import SyncTeamViewSet
 from micro_services.payments.views import PaymentViewSet
 from micro_services.users.views import UserViewSet
+from micro_services.mobile_payments.views import MobilePaymentViewSet
