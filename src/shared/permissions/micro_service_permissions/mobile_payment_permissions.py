ffda264b916dcacf4c46189d895ba36227522c87
Khai Tran <khaitq@cystack.net>
2022-03-07 11:34:56 +0700

    Update mobile payment webhook

--- src/shared/permissions/micro_service_permissions/mobile_payment_permissions.py
+++ src/shared/permissions/micro_service_permissions/mobile_payment_permissions.py
@@ -9,7 +9,7 @@ class MobilePaymentPermission(MicroServicePermission):
         return super(MobilePaymentPermission, self).has_permission(request, view)
 
     def has_object_permission(self, request, view, obj):
-        if view.action in ["upgrade_plan", "mobile_renewal", "mobile_destroy_subscription",
+        if view.action in ["upgrade_plan", "mobile_renewal", "mobile_cancel_subscription",
                            "mobile_destroy_subscription"]:
             return True
         return super(MobilePaymentPermission, self).has_object_permission(request, view, obj)
