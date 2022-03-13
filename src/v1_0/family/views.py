c074a3a5938dab613255763bcd9f78f71afee843
Tran Quang Khai <khai.tranquang.8797@gmail.com>
2022-03-13 22:08:46 +0700

    Update family members

--- src/v1_0/family/views.py
+++ src/v1_0/family/views.py
@@ -20,7 +20,7 @@ from shared.permissions.locker_permissions.user_pwd_permission import UserPwdPer
 from shared.services.pm_sync import SYNC_EVENT_MEMBER_ACCEPTED, PwdSync, SYNC_EVENT_VAULT, SYNC_EVENT_MEMBER_UPDATE
 from shared.utils.app import now
 from shared.utils.network import detect_device
-from v1_0.family.serializers import UserPlanFamilySerializer
+from v1_0.family.serializers import UserPlanFamilySerializer, CreateUserPlanFamilySeriaizer
 from v1_0.apps import PasswordManagerViewSet
 
 
@@ -31,6 +31,8 @@ class FamilyPwdViewSet(PasswordManagerViewSet):
     def get_serializer_class(self):
         if self.action == "member_list":
             self.serializer_class = UserPlanFamilySerializer
+        elif self.action == "member_create":
+            self.serializer_class = CreateUserPlanFamilySeriaizer
         return super(FamilyPwdViewSet, self).get_serializer_class()
 
     def get_object(self):
@@ -47,3 +49,27 @@ class FamilyPwdViewSet(PasswordManagerViewSet):
         serializer = self.get_serializer(family_members, many=True)
         return Response(status=200, data=serializer.data)
 
+    @action(methods=["get"], detail=False)
+    def member_create(self, request, *args, **kwargs):
+        pm_current_plan = self.get_object()
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        family_members = validated_data.get("family_members")
+
+        existed_family_members = pm_current_plan.pm_plan_family.all()
+        pm_plan = pm_current_plan.get_plan_obj()
+        if len(family_members) > pm_plan.get_max_number_members() - existed_family_members.count():
+            raise ValidationError(detail={"family_members": [
+                "The plan only accepts {} members including you".format(pm_plan.get_max_number_members())
+            ]})
+
+        for family_member in family_members:
+            email = family_member.get("email")
+            user_id = family_member.get("user_id")
+            self.user_repository.add_to_family_sharing(
+                family_user_plan=pm_current_plan, user_id=user_id, email=email
+            )
+
+        return Response(status=200, data={"success": True})
