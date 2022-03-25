ac45704b3132630753da50ac8e18b2a86786c664
Khai Tran <khaitq@cystack.net>
2022-03-25 09:50:37 +0700

    Apply trial plan for beta users

--- src/v1_0/users/serializers.py
+++ src/v1_0/users/serializers.py
@@ -22,7 +22,7 @@ class UserPwdSerializer(serializers.Serializer):
     master_password_hash = serializers.CharField(allow_blank=False)
     master_password_hint = serializers.CharField(allow_blank=True)
     score = serializers.FloatField(required=False, allow_null=True)
-    trial_plan = serializers.ChoiceField(choices=[PLAN_TYPE_PM_PREMIUM], required=False)
+    trial_plan = serializers.ChoiceField(choices=[PLAN_TYPE_PM_PREMIUM], required=False, default=PLAN_TYPE_PM_PREMIUM)
     team_key = serializers.CharField(required=False, allow_null=True)
     collection_name = serializers.CharField(required=False, allow_null=True)
 
