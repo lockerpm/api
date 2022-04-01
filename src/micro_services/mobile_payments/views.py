c610d0ca31bb174671fe277685b16271ea955938
Khai Tran <khaitq@cystack.net>
2022-04-01 15:18:23 +0700

    Update

--- src/micro_services/mobile_payments/views.py
+++ src/micro_services/mobile_payments/views.py
@@ -58,7 +58,7 @@ class MobilePaymentViewSet(MicroServiceViewSet):
                 pm_mobile_subscription=confirm_original_id
             )
             # Downgrade plan of the existed mobile original id
-            if mobile_original_id_registered.user_id != user.user_id:
+            if mobile_original_id_registered and mobile_original_id_registered.user_id != user.user_id:
                 self.user_repository.update_plan(
                     user=user, plan_type_alias=PLAN_TYPE_PM_FREE, scope=settings.SCOPE_PWD_MANAGER
                 )
