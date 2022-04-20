4d211b766846359891e6d79244523f3f616eb334
Khai Tran <khaitq@cystack.net>
2022-04-20 13:58:37 +0700

    Update delete, restore multiple ciphers

--- src/shared/background/implements/__init__.py
+++ src/shared/background/implements/__init__.py
@@ -1,2 +1,3 @@
 from shared.background.implements.notify_background import NotifyBackground
 from shared.background.implements.event_background import EventBackground
+from shared.background.implements.cipher_background import CipherBackground
