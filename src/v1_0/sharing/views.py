174a5739ad268727786c93c807d280a79da11f42
Khai Tran <khaitq@cystack.net>
2022-07-11 11:22:35 +0700

    feat: Remove item from a share folder

--- src/v1_0/sharing/views.py
+++ src/v1_0/sharing/views.py
@@ -48,7 +48,7 @@ class SharingPwdViewSet(PasswordManagerViewSet):
         elif self.action == "add_member":
             self.serializer_class = AddMemberSerializer
 
-        elif self.action == "add_item_share_folder":
+        elif self.action in ["add_item_share_folder", "remove_item_share_folder"]:
             self.serializer_class = AddItemShareFolderSerializer
         return super(SharingPwdViewSet, self).get_serializer_class()
 
@@ -763,5 +763,43 @@ class SharingPwdViewSet(PasswordManagerViewSet):
             team=personal_share,
             add_all=True
         ).send(data={"id": cipher.id})
+
+        return Response(status=200, data={"success": True})
+
+    @action(methods=["put"], detail=False)
+    def remove_item_share_folder(self, request, *args, **kwargs):
+        user = self.request.user
+        self.check_pwd_session_auth(request)
+        personal_share = self.get_personal_share(sharing_id=kwargs.get("pk"))
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.save()
+        cipher_data = validated_data.get("cipher")
+        # Get collection of the team
+        try:
+            collection_obj = self.collection_repository.get_team_collection_by_id(
+                collection_id=kwargs.get("folder_id"), team_id=personal_share.id
+            )
+        except ObjectDoesNotExist:
+            raise ValidationError(detail={"folder": ["The folder does not exist"]})
+
+        try:
+            cipher_obj = self.cipher_repository.get_by_id(cipher_id=cipher_data.get("id"))
+            if cipher_obj.team_id != personal_share.id:
+                raise ValidationError(detail={"cipher": {"id": ["The cipher id does not exist"]}})
+        except ObjectDoesNotExist:
+            raise ValidationError(detail={"cipher": {"id": ["The cipher id does not exist"]}})
+
+        cipher_data["team_id"] = None
+        cipher_data["collection_ids"] = []
+        cipher_data["user_id"] = user.user_id
+        cipher_data = json.loads(json.dumps(cipher_data))
+        cipher = self.cipher_repository.save_share_cipher(cipher=cipher_obj, cipher_data=cipher_data)
+        collection_obj.revision_date = now()
+        collection_obj.save()
+
+        PwdSync(event=SYNC_EVENT_CIPHER_UPDATE, user_ids=[user.user_id], team=personal_share, add_all=True).send(
+            data={"id": cipher.id}
+        )
 
         return Response(status=200, data={"success": True})
--- src/v1_0/urls.py
+++ src/v1_0/urls.py
@@ -123,8 +123,7 @@ urlpatterns += [
         views.SharingPwdViewSet.as_view({'post': 'stop_share_folder'})),
 
     url(r'^sharing/(?P<pk>[0-9]+)/folders/(?P<folder_id>[0-9a-z\-]+)/items$',
-        views.SharingPwdViewSet.as_view({'post': 'add_item_share_folder'})),
-
+        views.SharingPwdViewSet.as_view({'post': 'add_item_share_folder', 'put': 'remove_item_share_folder'})),
     url(r'^sharing/my_share$', views.SharingPwdViewSet.as_view({'get': 'my_share'})),
 
 ]
