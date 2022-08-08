810fa28488f0852764d8cf19d8a49d3d1eeecc58
Khai Tran <khaitq@cystack.net>
2022-08-08 14:51:18 +0700

    chore: Add owner in list enterprise invitations

--- src/v1_enterprise/enterprise_members/serializers.py
+++ src/v1_enterprise/enterprise_members/serializers.py
@@ -49,6 +49,7 @@ class UserInvitationSerializer(serializers.ModelSerializer):
             "id": instance.enterprise_id,
             "name": instance.enterprise.name
         }
+        data["owner"] = instance.enterprise.enterprise_members.get(is_primary=True).user_id
         if instance.domain is not None:
             data["domain"] = {
                 "id": instance.domain.id,
