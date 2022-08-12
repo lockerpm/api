aa9598e8627b043b754430803fbefebe81ff04e9
Tran Quang Khai <khai.tranquang.8797@gmail.com>
2022-08-12 22:24:18 +0700

    chore: Update activity log filter

--- src/v1_enterprise/enterprise_activity_logs/views.py
+++ src/v1_enterprise/enterprise_activity_logs/views.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
 from rest_framework.exceptions import NotFound, ValidationError
 
 from cystack_models.models.enterprises.enterprises import Enterprise
 from cystack_models.models.events.events import Event
+from shared.constants.enterprise_members import *
+from shared.constants.event import *
 from shared.error_responses.error import gen_error
 from shared.permissions.locker_permissions.enterprise.activity_log_permission import ActivityLogPwdPermission
 from shared.utils.app import now
@@ -38,13 +41,49 @@ class ActivityLogPwdViewSet(EnterpriseViewSet):
 
     def get_queryset(self):
         enterprise = self.get_enterprise()
+        enterprise = Enterprise.objects.get()
         to_param = self.check_int_param(self.request.query_params.get("to")) or now()
         from_param = self.check_int_param(self.request.query_params.get("from")) or now() - 30 * 86400
+        admin_only_param = self.request.query_params.get("admin_only", "0")
+        member_only_param = self.request.query_params.get("member_only", "0")
+        group_param = self.request.query_params.get("group")
+        member_ids_param = self.request.query_params.get("member_ids")
+        action_param = self.request.query_params.get("action")
 
         events = Event.objects.filter(team_id=enterprise.id).order_by('-creation_date').filter(
             creation_date__lte=to_param,
             creation_date__gte=from_param
         )
+        if admin_only_param == "1":
+            admin_user_ids = list(enterprise.enterprise_members.filter(
+                role_id__in=[E_MEMBER_ROLE_PRIMARY_ADMIN, E_MEMBER_ROLE_ADMIN]
+            ).values_list('user_id', flat=True))
+            events = events.filter(Q(acting_user_id__in=admin_user_ids) | Q(user_id__in=admin_user_ids)).distinct()
+        if member_only_param == "1":
+            member_user_ids = list(enterprise.enterprise_members.filter(
+                role_id__in=[E_MEMBER_ROLE_MEMBER]
+            ).values_list('user_id', flat=True))
+            events = events.filter(Q(acting_user_id__in=member_user_ids) | Q(user_id__in=member_user_ids)).distinct()
+        if member_ids_param:
+            member_user_ids = list(enterprise.enterprise_members.filter(
+                id__in=member_ids_param.split(",")
+            ).values_list('user_id', flat=True))
+            events = events.filter(Q(acting_user_id__in=member_user_ids) | Q(user_id__in=member_user_ids)).distinct()
+        if group_param:
+            member_user_ids = list(
+                enterprise.groups.filter(id=group_param).values_list('groups_members__member__user_id', flat=True)
+            )
+            events = events.filter(Q(acting_user_id__in=member_user_ids) | Q(user_id__in=member_user_ids)).distinct()
+        if action_param:
+            if action_param == "member_changes":
+                events = events.filter(type__in=[
+                    EVENT_E_MEMBER_INVITED, EVENT_E_MEMBER_CONFIRMED, EVENT_E_MEMBER_REMOVED,
+                    EVENT_E_MEMBER_UPDATED_GROUP, EVENT_E_MEMBER_ENABLED, EVENT_E_MEMBER_DISABLED
+                ])
+            elif action_param == "role_changes":
+                events = events.filter(type__in=[EVENT_E_MEMBER_UPDATED_ROLE])
+            elif action_param == "policy_violations":
+                events = events.filter(type__in=[EVENT_USER_BLOCK_LOGIN])
         return events
 
     def list(self, request, *args, **kwargs):
