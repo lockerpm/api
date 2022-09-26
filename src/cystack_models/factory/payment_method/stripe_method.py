7f8fbb4bc56dda419cd895223b8345c84fbe8b5c
Khai Tran <khaitq@cystack.net>
2022-09-26 12:00:20 +0700

    chore: Re-map stripe plan

--- src/cystack_models/factory/payment_method/stripe_method.py
+++ src/cystack_models/factory/payment_method/stripe_method.py
@@ -335,6 +335,8 @@ class StripePaymentMethod(IPaymentMethod):
         real_stripe_plans = {}
         if os.getenv("PROD_ENV") == "staging":
             real_stripe_plans["pm_enterprise_monthly"] = "locker_pm_enterprise_monthly"
+        elif os.getenv("PROD_ENV") == "prod":
+            real_stripe_plans["pm_family_yearly"] = "locker_pm_family_yearly"
 
         return real_stripe_plans.get(stripe_plan_id, stripe_plan_id)
 
