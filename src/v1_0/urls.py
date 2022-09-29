2a683b8d9112bf5849c20e16d60fcb2b7db1812b
Khai Tran <khaitq@cystack.net>
2022-09-29 12:31:11 +0700

    feat: Add passwordless cred API

--- src/v1_0/urls.py
+++ src/v1_0/urls.py
@@ -51,6 +51,7 @@ urlpatterns += [
 urlpatterns += [
     url(r'^users/me$', views.UserPwdViewSet.as_view({'get': 'me', 'put': 'me'})),
     url(r'^users/me/revision_date$', views.UserPwdViewSet.as_view({'get': 'revision_date'})),
+    url(r'^users/me/login_method$', views.UserPwdViewSet.as_view({'get': 'login_method_me'})),
     url(r'^users/me/violation$', views.UserPwdViewSet.as_view({'get': 'violation_me'})),
     url(r'^users/me/family', views.UserPwdViewSet.as_view({'get': 'family'})),
     url(r'^users/me/delete$', views.UserPwdViewSet.as_view({'post': 'delete_me'})),
