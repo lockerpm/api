2190e0e3222ca5084c7cf6be720dc60f6bc509b7
Khai Tran <khaitq@cystack.net>
2022-10-08 13:24:36 +0700

    fix: Fix user permissions

--- src/shared/permissions/locker_permissions/user_pwd_permission.py
+++ src/shared/permissions/locker_permissions/user_pwd_permission.py
@@ -5,8 +5,10 @@ class UserPwdPermission(LockerPermission):
     def has_permission(self, request, view):
         if view.action in ["password_hint"]:
             return True
-        if view.action in ["register", "prelogin", "me", "session"]:
+        if view.action in ["prelogin", "me", "session"]:
             return self.is_auth(request)
+        elif view.action in ["register"]:
+            return self.is_auth(request) and request.user.activated is False
         elif view.action in ["retrieve", "dashboard", "list_users", "list_user_ids", "destroy"]:
             return self.is_admin(request)
         return self.is_auth(request) and request.user.activated
