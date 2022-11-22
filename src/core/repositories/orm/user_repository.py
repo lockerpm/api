c3280ad3bb3b16010fbc7e8682c7f9c00b5ca308
Khai Tran <khaitq@cystack.net>
2022-11-22 17:54:06 +0700

    chore: Update list invitations

--- src/core/repositories/orm/user_repository.py
+++ src/core/repositories/orm/user_repository.py
@@ -646,10 +646,10 @@ class UserRepository(IUserRepository):
 
         return data_customer_stripe
 
-
     def get_list_invitations(self, user: User, personal_share=False):
         member_invitations = user.team_members.filter(
-            status__in=[PM_MEMBER_STATUS_INVITED, PM_MEMBER_STATUS_ACCEPTED], team__personal_share=personal_share
+            status__in=[PM_MEMBER_STATUS_INVITED, PM_MEMBER_STATUS_ACCEPTED], team__personal_share=personal_share,
+            is_added_by_group=False
         ).select_related('team').order_by('access_time')
         return member_invitations
 
