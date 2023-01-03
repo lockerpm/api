72d0693641ca8e5537b86416059a9193965bb313
Khai Tran <khaitq@cystack.net>
2023-01-03 11:39:27 +0700

    chore: Delete old events cron job

---
+++ src/cron/tasks/delete_old_events.py
@@ -0,0 +1,29 @@
+import time
+import schedule
+
+from cron.task import Task
+from cystack_models.models.events.events import Event
+from shared.utils.app import now
+
+
+class DeleteOldEvents(Task):
+    def __init__(self):
+        super(DeleteOldEvents, self).__init__()
+        self.job_id = 'delete_old_events'
+
+    def register_job(self):
+        pass
+
+    def log_job_execution(self, run_time: float, exception: str = None, tb: str = None):
+        pass
+
+    def real_run(self, *args):
+        current_time = now()
+        # Delete old events if the creation date is less than 90 days
+        Event.objects.filter(creation_date__lte=current_time - 90 * 86400).delete()
+
+    def scheduling(self):
+        schedule.every().day.at("17:00").do(self.run)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
