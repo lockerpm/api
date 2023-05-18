63f13c5a626e1b00e73f204c3d72e19369c36aab
Khai Tran <khaitq@cystack.net>
2023-05-18 14:11:45 +0700

    chore: Update promo code

--- src/v1_0/user_rewards/serializers.py
+++ src/v1_0/user_rewards/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from cystack_models.models.payments.promo_codes import PromoCode
 from cystack_models.models.user_rewards.user_reward_missions import UserRewardMission
 from v1_0.resources.serializers import RewardMissionSerializer
 
@@ -18,3 +19,18 @@ class UserRewardMissionSerializer(serializers.ModelSerializer):
 
 class UserRewardCheckCompletedSerializer(serializers.Serializer):
     user_identifier = serializers.CharField(max_length=255)
+
+
+class ListRewardPromoCodeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PromoCode
+        fields = ('id', 'created_time', 'expired_time', 'valid', 'code', 'value')
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["description"] = {
+            "vi": instance.description_vi,
+            "en": instance.description_en
+        }
+        data["type"] = instance.type_id
+        return data
