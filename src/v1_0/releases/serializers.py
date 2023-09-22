5206da32142de36349a91c4fffafb478374d4c61
Khai Tran <khaitq@cystack.net>
2023-09-22 15:39:46 +0700

    chore: Update release api

--- src/v1_0/releases/serializers.py
+++ src/v1_0/releases/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 
-from shared.constants.device_type import CLIENT_ID_BROWSER, CLIENT_ID_DESKTOP, CLIENT_ID_CLI
+from shared.constants.device_type import *
 
 
 class NewReleaseSerializer(serializers.Serializer):
     build = serializers.BooleanField()
     client_id = serializers.ChoiceField(
-        choices=[CLIENT_ID_BROWSER, CLIENT_ID_DESKTOP, CLIENT_ID_CLI],
+        choices=[CLIENT_ID_BROWSER, CLIENT_ID_DESKTOP, CLIENT_ID_CLI,
+                 CLIENT_ID_SDK_PYTHON, CLIENT_ID_SDK_NODEJS, CLIENT_ID_SDK_DOTNET],
         default=CLIENT_ID_DESKTOP
     )
     environment = serializers.ChoiceField(choices=["prod", "staging"], default="prod")
@@ -14,7 +15,8 @@ class NewReleaseSerializer(serializers.Serializer):
 
 class NextReleaseSerializer(serializers.Serializer):
     client_id = serializers.ChoiceField(
-        choices=[CLIENT_ID_BROWSER, CLIENT_ID_DESKTOP, CLIENT_ID_CLI],
+        choices=[CLIENT_ID_BROWSER, CLIENT_ID_DESKTOP, CLIENT_ID_CLI,
+                 CLIENT_ID_SDK_PYTHON, CLIENT_ID_SDK_NODEJS, CLIENT_ID_SDK_DOTNET],
         default=CLIENT_ID_DESKTOP
     )
     environment = serializers.ChoiceField(choices=["prod", "staging"], default="prod")
